8ab3a9ff543bd172bed6013bcb4c557538f7b431
caleb <caleb@prowebmarketing.com>
2016-06-15 01:26:12 -0400

    socket.io

--- includes/socket.io/dispatcher.js
+++ includes/socket.io/dispatcher.js
@@ -14,21 +14,21 @@ global.socketIoDispatcher = function(client) {
         });
     }
     
-//    client.on('join', function(data) {
-//        console.log(data);
-//    });
-//    
-//    client.on('messages', function(data) {
-//        client.emit('broad', data);
-//        client.broadcast.emit('broad',data);
-//    });
-//    
-//    // If we recieved a command from a client to start watering lets do so
-//    client.on('ping', function (data) {
-//        console.log("ping");
-//
-//        delay = data["duration"];
-//        client.emit("pong", data);
-//        client.broadcast.emit('pong', data);
-//    });
+    client.on('join', function(data) {
+        console.log(data);
+    });
+    
+    client.on('messages', function(data) {
+        client.emit('broad', data);
+        client.broadcast.emit('broad',data);
+    });
+    
+    // If we recieved a command from a client to start watering lets do so
+    client.on('ping', function (data) {
+        console.log("ping");
+
+        delay = data["duration"];
+        client.emit("pong", data);
+        client.broadcast.emit('pong', data);
+    });
 };
