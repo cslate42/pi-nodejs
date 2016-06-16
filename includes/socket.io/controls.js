5c4e780b508d20e05f8e1db043998f5dd32fe242
caleb <caleb@prowebmarketing.com>
2016-06-16 17:23:42 -0400

    Update

--- includes/socket.io/controls.js
+++ includes/socket.io/controls.js
@@ -40,64 +40,64 @@ global.socketIoClients['update-controls'] = function (client, data) {
 };
 
 function chassisForwardLeft() {
-    gpio.pin(gpio.const.lMotorBack, 1);
-    gpio.pin(gpio.const.lMotorFor, 0);
-    gpio.pin(gpio.const.rMotorBack, 0);
-    gpio.pin(gpio.const.rMotorFor, 1);
+    gpio.pin(gpio.CONST.lMotorBack, 1);
+    gpio.pin(gpio.CONST.lMotorFor, 0);
+    gpio.pin(gpio.CONST.rMotorBack, 0);
+    gpio.pin(gpio.CONST.rMotorFor, 1);
 }
 
 function chassisForwardRight() {
-    gpio.pin(gpio.const.lMotorBack, 1);
-    gpio.pin(gpio.const.lMotorFor, 0);
-    gpio.pin(gpio.const.rMotorBack, 0);
-    gpio.pin(gpio.const.rMotorFor, 1);
+    gpio.pin(gpio.CONST.lMotorBack, 1);
+    gpio.pin(gpio.CONST.lMotorFor, 0);
+    gpio.pin(gpio.CONST.rMotorBack, 0);
+    gpio.pin(gpio.CONST.rMotorFor, 1);
 }
 
 function chassisBackwardLeft() {
-    gpio.pin(gpio.const.lMotorBack, 1);
-    gpio.pin(gpio.const.lMotorFor, 0);
-    gpio.pin(gpio.const.rMotorBack, 0);
-    gpio.pin(gpio.const.rMotorFor, 1);
+    gpio.pin(gpio.CONST.lMotorBack, 1);
+    gpio.pin(gpio.CONST.lMotorFor, 0);
+    gpio.pin(gpio.CONST.rMotorBack, 0);
+    gpio.pin(gpio.CONST.rMotorFor, 1);
 }
 
 function chassisBackwardRight() {
-    gpio.pin(gpio.const.lMotorBack, 1);
-    gpio.pin(gpio.const.lMotorFor, 0);
-    gpio.pin(gpio.const.rMotorBack, 0);
-    gpio.pin(gpio.const.rMotorFor, 1);
+    gpio.pin(gpio.CONST.lMotorBack, 1);
+    gpio.pin(gpio.CONST.lMotorFor, 0);
+    gpio.pin(gpio.CONST.rMotorBack, 0);
+    gpio.pin(gpio.CONST.rMotorFor, 1);
 }
 
 function chassisForward() {
-    gpio.pin(gpio.const.lMotorBack, 1);
-    gpio.pin(gpio.const.lMotorFor, 0);
-    gpio.pin(gpio.const.rMotorBack, 0);
-    gpio.pin(gpio.const.rMotorFor, 1);
+    gpio.pin(gpio.CONST.lMotorBack, 1);
+    gpio.pin(gpio.CONST.lMotorFor, 0);
+    gpio.pin(gpio.CONST.rMotorBack, 0);
+    gpio.pin(gpio.CONST.rMotorFor, 1);
 }
 
 function chassisRight() {
-    gpio.pin(gpio.const.lMotorBack, 1);
-    gpio.pin(gpio.const.lMotorFor, 0);
-    gpio.pin(gpio.const.rMotorBack, 0);
-    gpio.pin(gpio.const.rMotorFor, 1);
+    gpio.pin(gpio.CONST.lMotorBack, 1);
+    gpio.pin(gpio.CONST.lMotorFor, 0);
+    gpio.pin(gpio.CONST.rMotorBack, 0);
+    gpio.pin(gpio.CONST.rMotorFor, 1);
 }
 
 function chassisBackward() {
-    gpio.pin(gpio.const.lMotorBack, 1);
-    gpio.pin(gpio.const.lMotorFor, 0);
-    gpio.pin(gpio.const.rMotorBack, 0);
-    gpio.pin(gpio.const.rMotorFor, 1);
+    gpio.pin(gpio.CONST.lMotorBack, 1);
+    gpio.pin(gpio.CONST.lMotorFor, 0);
+    gpio.pin(gpio.CONST.rMotorBack, 0);
+    gpio.pin(gpio.CONST.rMotorFor, 1);
 }
 
 function chassisLeft() {
-    gpio.pin(gpio.const.lMotorBack, 1);
-    gpio.pin(gpio.const.lMotorFor, 0);
-    gpio.pin(gpio.const.rMotorBack, 0);
-    gpio.pin(gpio.const.rMotorFor, 1);
+    gpio.pin(gpio.CONST.lMotorBack, 1);
+    gpio.pin(gpio.CONST.lMotorFor, 0);
+    gpio.pin(gpio.CONST.rMotorBack, 0);
+    gpio.pin(gpio.CONST.rMotorFor, 1);
 }
 
 function chassisStop() {
-    gpio.pin(gpio.const.lMotorBack, 0);
-    gpio.pin(gpio.const.lMotorFor, 0);
-    gpio.pin(gpio.const.rMotorBack, 0);
-    gpio.pin(gpio.const.rMotorFor, 0);
+    gpio.pin(gpio.CONST.lMotorBack, 0);
+    gpio.pin(gpio.CONST.lMotorFor, 0);
+    gpio.pin(gpio.CONST.rMotorBack, 0);
+    gpio.pin(gpio.CONST.rMotorFor, 0);
 }
